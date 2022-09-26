59d2be0a16783c23bebd6e64eaa7c2d1eda74831
Luan Pinto <luan.nelmo@hotmail.com>
2022-09-25 21:32:44 -0300

    Aplicando validacoes Simples

--- src/main/java/com/curso/springboot/controllers/ProdutoController.java
+++ src/main/java/com/curso/springboot/controllers/ProdutoController.java
@@ -3,9 +3,12 @@ package com.curso.springboot.controllers;
 import com.curso.springboot.model.entities.Produto;
 import com.curso.springboot.model.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -14,7 +17,8 @@ public class ProdutoController {
     @Autowired
     private ProdutoRepository produtoRepository;
 
-    public Produto novoProduto(Produto produto) {
+    @PostMapping
+    public Produto novoProduto(@Valid Produto produto) {
         produtoRepository.save(produto);
         return produto;
     }
