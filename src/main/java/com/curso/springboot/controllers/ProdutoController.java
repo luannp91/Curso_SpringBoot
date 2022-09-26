c66237438f58fd32dea090361aa2acb120d820b9
Luan Pinto <luan.nelmo@hotmail.com>
2022-09-25 21:42:10 -0300

    Consultando Produtos por ID

--- src/main/java/com/curso/springboot/controllers/ProdutoController.java
+++ src/main/java/com/curso/springboot/controllers/ProdutoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -23,5 +24,10 @@ public class ProdutoController {
     @GetMapping
     public Iterable<Produto> obterProdutos() {
         return produtoRepository.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<Produto> obterProdutoPorId(@PathVariable int id) {
+        return produtoRepository.findById(id);
     }
 }
